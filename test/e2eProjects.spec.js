8af90c68c574b8f45f48ac66c100c9c1a3937a49
Egor I <egor.ivanov@pm.me>
2025-08-02 20:41:37 +0200

    fix issue IDs
    Signed-off-by: Egor I <egor.ivanov@pm.me>

--- test/e2eProjects.spec.js
+++ test/e2eProjects.spec.js
@@ -18,7 +18,7 @@ test.beforeEach(async () => {
 test("Authenticated user must be able to create a new project", async () => {
   await allure.story("Authenticated user can manage projects");
   await allure.tags("web", "critical", "regress");
-  await attachJiraIssue("AD-5");
+  await attachJiraIssue("AD-4");
   await allure.layer("e2e");
   await allure.owner("bugsbunny");
   await allure.feature("Projects");
@@ -30,7 +30,7 @@ test("Authenticated user must be able to create a new project", async () => {
 test("Authenticated and properly authorized user must be able to delete an existing project", async () => {
   await allure.story("Authenticated user can manage projects");
   await allure.tags("web", "regress");
-  await attachJiraIssue("AD-6");
+  await attachJiraIssue("AD-5");
   await attachMicroservice("testops");
   await allure.layer("e2e");
   await allure.owner("bugsbunny");
