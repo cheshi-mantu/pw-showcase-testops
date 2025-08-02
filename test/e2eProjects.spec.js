f68e44ffcee2d9b165b15c677a96aedc7e9bb5c8
Egor I <egor.ivanov@pm.me>
2025-08-02 20:44:06 +0200

    adds epic
    Signed-off-by: Egor I <egor.ivanov@pm.me>

--- test/e2eProjects.spec.js
+++ test/e2eProjects.spec.js
@@ -16,25 +16,27 @@ test.beforeEach(async () => {
 });
 
 test("Authenticated user must be able to create a new project", async () => {
+  await allure.epic("Projects");
+  await allure.feature("Managing Projects");
   await allure.story("Authenticated user can manage projects");
   await allure.tags("web", "critical", "regress");
   await attachJiraIssue("AD-4");
   await allure.layer("e2e");
   await allure.owner("bugsbunny");
-  await allure.feature("Projects");
   await attachMicroservice("testops");
   await authorize();
   await createNewEntity("Project");
 });
 
 test("Authenticated and properly authorized user must be able to delete an existing project", async () => {
+  await allure.epic("Projects");
+  await allure.feature("Managing Projects");
   await allure.story("Authenticated user can manage projects");
   await allure.tags("web", "regress");
   await attachJiraIssue("AD-5");
   await attachMicroservice("testops");
   await allure.layer("e2e");
   await allure.owner("bugsbunny");
-  await allure.feature("Projects");
   await authorize();
   await deleteNewEntity("Project");
 });
